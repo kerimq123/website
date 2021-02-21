1b705820c1737af2dea6d955484dc116779c2e6d
kerimq123 <79404510+kerimq123@users.noreply.github.com>
2021-02-21 15:20:34 +0300

    Add files via upload

---
+++ panzerorj.php
@@ -0,0 +1,94 @@
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+</head>
+<style>
+
+    .center {
+      
+      margin: 0 auto;
+      
+    }
+
+    .awesome {
+      
+      color:#313131;
+      -webkit-animation:colorchange 10s infinite alternate;
+      
+      
+    }
+
+    @-webkit-keyframes colorchange {
+      0% {
+        
+        color: blue;
+      }
+      
+      10% {
+        
+        color: #8e44ad;
+      }
+      
+      20% {
+        
+        color: #1abc9c;
+      }
+      
+      30% {
+        
+        color: #d35400;
+      }
+      
+      40% {
+        
+        color: blue;
+      }
+      
+      50% {
+        
+        color: #34495e;
+      }
+      
+      60% {
+        
+        color: blue;
+      }
+      
+      70% {
+        
+        color: #2980b9;
+      }
+      80% {
+     
+        color: #f1c40f;
+      }
+      
+      90% {
+     
+        color: #2980b9;
+      }
+      
+      100% {
+        
+        color: pink;
+      }
+    }
+</style>
+<body bgcolor='#000000'>
+<body bgcolor='rgb(0,0,0)'>
+<body bgcolor='black'>
+<hr>
+<font class='awesome' style='font-family:Arial'>
+<font color='red'>Kullanıcı Adı: </font><font color='white'>ohainsan</font><br>
+<font color='red'> Şifre: </font><font color='white'>panzer123</font><br>
+<font color='red'>Ip Adresi: </font><font color='white'>::1</font><br>
+<font color='red'>Tarih: </font><font color='white'>28-01-2021 20:01:17</font><br>
+<font color='red'>Ülke: </font><font color='white'></font><br>
+<font color='red'>Şehir: </font><font color='white'></font><br>
+</font>
+<hr>
+<br>
+
